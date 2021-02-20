425992ec46a5b8c7c93c7812d98002f1fbdc4555
CihanKocak <69902606+CihanKocak@users.noreply.github.com>
2021-02-20 17:21:24 +0300

    Autofac,FluentValidation,AOP added

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,12 +23,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            if (car.DailyPrice > 0)
-            {
-                return new ErrorResult(Messages.CarDailyPriceInvalid);              
-            }
             _carDal.Add(car);
 
             return new SuccessResult(Messages.CarAdded);
@@ -39,7 +40,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
             }
---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,51 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.FirstName).Length(3, 15);
+            RuleFor(u => u.FirstName).Must(u => HasValidFirstName(u)).WithMessage("The Firstname must only contain letters");
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.LastName).Length(3, 15);
+            RuleFor(u => u.LastName).Must(u => HasValidLastName(u)).WithMessage("The Lastname must only contain letters");
+            RuleFor(u => u.Email).NotEmpty();
+            RuleFor(u => u.Email).EmailAddress();
+            RuleFor(u => u.Password).NotEmpty();
+            RuleFor(u => u.Password).MinimumLength(8);
+            RuleFor(u => u.Password).Must(u => HasValidPassword(u)).WithMessage("The Password must contain alphanumeric characters");
+        }
+
+        private bool HasValidFirstName(string args)
+        {
+            var lowercase = new Regex("[a-z]+");
+            var uppercase = new Regex("[A-Z]+");
+
+            return (lowercase.IsMatch(args) && uppercase.IsMatch(args));
+        }
+        private bool HasValidLastName(string args)
+        {
+            var lowercase = new Regex("[a-z]+");
+            var uppercase = new Regex("[A-Z]+");
+
+            return (lowercase.IsMatch(args) && uppercase.IsMatch(args));
+        }
+        private bool HasValidPassword(string args)
+        {
+            var lowercase = new Regex("[a-z]+");
+            var uppercase = new Regex("[A-Z]+");
+            var digit = new Regex("(\\d)+");
+            var symbol = new Regex("(\\W)+");
+
+            return (lowercase.IsMatch(args) && uppercase.IsMatch(args) && digit.IsMatch(args) && symbol.IsMatch(args));
+        }
+    }
+}
